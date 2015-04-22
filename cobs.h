0ff63821f97979675b39491b97b465d67f139469
Spacebird1313 <huybthomas@gmail.com>
2015-04-22 14:09:53 +0200

    Added cobs encoding/decoding with zero pair elimination

---
+++ cobs.h
@@ -0,0 +1,12 @@
+#ifndef COBS
+#define COBS
+
+int encodeCOBS(unsigned char* inBuffer, int length, unsigned char* outBuffer);
+
+int decodeCOBS(unsigned char* inBuffer, int length, unsigned char* outBuffer);
+
+int encodeCOBSZPE(unsigned char* inBuffer, int length, unsigned char* outBuffer);
+
+int decodeCOBSZPE(unsigned char* inBuffer, int length, unsigned char* outBuffer);
+
+#endif
